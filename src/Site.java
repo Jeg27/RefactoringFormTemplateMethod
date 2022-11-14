b1deca07f76bdf4d82dc9ff651395c3507e8375f
james <jamesgrieder@cmail.carleton.ca>
2022-11-14 15:20:37 -0500

    Initial commit - Site example for the Form Template Method

---
+++ src/Site.java
@@ -0,0 +1,12 @@
+
+
+public class Main {
+
+    static String getValueForField(String fieldName, String message) {
+        final int indexOfFieldValue = message.indexOf(fieldName) + fieldName.length();
+        final int indexOfEndOfFieldValue = message.indexOf("\"", indexOfFieldValue);
+        return message.substring(indexOfFieldValue, indexOfEndOfFieldValue);
+    }
+
+
+}
