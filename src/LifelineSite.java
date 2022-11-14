920e268e3f82de1bb439017c2e850fc6cdb7b862
james <jamesgrieder@cmail.carleton.ca>
2022-11-14 15:37:50 -0500

    Starting Point

--- src/LifelineSite.java
+++ src/LifelineSite.java
@@ -12,7 +12,7 @@ public class LifelineSite extends Site {
 
     public double getBillableAmount() {
         double base = _units * _rate * 0.5;
-        double tax = base * Site.TAX_RATE;
+        double tax = base * Site.TAX_RATE * 0.2;
         return base + tax;
     }
 }
